dfc6045903d79d24765a7d84a25c78d0aea9d857
Rai <jraicr@gmail.com>
2023-01-26 01:49:08 +0000

    Change Laravel container entry point to serve only with artisan

--- .devcontainer/webapp/docker-entrypoint.sh
+++ .devcontainer/webapp/docker-entrypoint.sh
@@ -4,11 +4,12 @@
 #php-fpm 
 
 # Artisan serves Laravel APP by default (Recommended to be used in development environment)
-#php artisan serve --host=0.0.0.0 --port=${WEB_APP_LISTENING_PORT}
-#php artisan serve
-# This command allows you to run php-fpm and artisan dev server at same time (Only for testing purposes from dev environment)
-php-fpm &
 php artisan serve --host=0.0.0.0 --port=${WEB_APP_LISTENING_PORT}
+#php artisan serve
+
+# This command allows you to run php-fpm and artisan dev server at same time (Only for testing purposes from dev environment)
+#php-fpm &
+#php artisan serve --host=0.0.0.0 --port=${WEB_APP_LISTENING_PORT}
 
 # Artisan serves Laravel APP using Octane with Swoole. (Recommended to be used in production & development environment)
 #composer require laravel/octane
