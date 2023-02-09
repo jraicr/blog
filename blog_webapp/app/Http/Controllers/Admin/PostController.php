dc0b396528d8990f8a60cd98433614d3c03215b5
Rai <jraicr@gmail.com>
2023-02-09 19:40:13 +0000

    Add posts remove feature. Fix auth security problem when creating post
    - The image from the removed posts are removed by an observer event.
    - Remove hidden input and control auth user only from the backend, via observer event when a post is created.

--- blog_webapp/app/Http/Controllers/Admin/PostController.php
+++ blog_webapp/app/Http/Controllers/Admin/PostController.php
@@ -131,6 +131,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index', $post)->with('info', 'El post se eliminó con éxito');
     }
 }
--- blog_webapp/resources/views/admin/posts/index.blade.php
+++ blog_webapp/resources/views/admin/posts/index.blade.php
@@ -3,11 +3,18 @@
 @section('title', 'Code Rai')
 
 @section('content_header')
-    <a class="float-right btn btn-secondary btn-sm" href="{{route('admin.posts.create')}}">Nuevo Post </a>
+    <a class="float-right btn btn-secondary btn-sm" href="{{ route('admin.posts.create') }}">Nuevo Post </a>
     <h1>Listado de posts</h1>
 @stop
 
 @section('content')
+
+    @if (session('info'))
+        <div class="alert alert-success">
+            <strong>{{ session('info') }}</strong>
+        </div>
+    @endif
+
     @livewire('admin.posts-index')
 @stop
 
