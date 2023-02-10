37d335b0927abe24f16d4ff5f15a9b61ff1dc6dc
Rai <jraicr@gmail.com>
2023-02-10 17:41:54 +0000

    Add route to create symlink

--- blog_webapp/routes/admin.php
+++ blog_webapp/routes/admin.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
@@ -14,3 +15,6 @@ Route::resource('tags', TagController::class)->names("admin.tags");
 
 Route::resource('posts', PostController::class)->names("admin.posts");
 
+Route::get('slc', function () {
+    Artisan::call('storage:link');
+});
