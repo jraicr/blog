4d6e3854ad7f529b5403a082ab84309cbbffb8a2
Rai <jraicr@gmail.com>
2023-02-23 14:02:22 +0000

    Add roles and permissions, roles CRUD and protected routes

--- blog_webapp/app/Http/Controllers/Admin/TagController.php
+++ blog_webapp/app/Http/Controllers/Admin/TagController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('can:admin.tags.index')->only('index');
+        $this->middleware('can:admin.tags.create')->only('create', 'store');
+        $this->middleware('can:admin.tags.edit')->only('edit', 'update');
+        $this->middleware('can:admin.tags.destroy')->only('destroy');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -55,17 +64,6 @@ class TagController extends Controller
         $tag = Tag::create($request->all());
 
         return redirect()->route('admin.tags.edit', compact("tag"))->with('info', 'La etiqueta se creó con éxito');;
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tag $tag)
-    {
-        return view('admin.tags.show', compact('tag'));
     }
 
     /**
--- blog_webapp/resources/views/admin/tags/index.blade.php
+++ blog_webapp/resources/views/admin/tags/index.blade.php
@@ -4,7 +4,10 @@
 
 @section('content_header')
 
-    <a class="float-right btn btn-secondary btn-sm" href="{{ route('admin.tags.create') }}">Nueva etiqueta</a>
+    @can('admin.tags.create')
+        <a class="float-right btn btn-secondary btn-sm" href="{{ route('admin.tags.create') }}">Nueva etiqueta</a>
+    @endcan
+
     <h1>Mostrar listado de etiqueta</h1>
 @stop
 
@@ -34,15 +37,19 @@
                             <td>{{ $tag->id }}</td>
                             <td>{{ $tag->name }}</td>
                             <td width="10px">
-                                <a class="btn btn-primary btn-sm" href="{{ route('admin.tags.edit', $tag) }}">Editar</a>
+                                @can('admin.tags.edit')
+                                    <a class="btn btn-primary btn-sm" href="{{ route('admin.tags.edit', $tag) }}">Editar</a>
+                                @endcan
                             </td>
                             <td width="10px">
-                                <form action="{{ route('admin.tags.destroy', $tag) }}" method="POST">
-                                    @csrf
-                                    @method('delete')
+                                @can('admin.tags.destroy')
+                                    <form action="{{ route('admin.tags.destroy', $tag) }}" method="POST">
+                                        @csrf
+                                        @method('delete')
 
-                                    <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
-                                </form>
+                                        <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                                    </form>
+                                @endcan
                             </td>
                         </tr>
                     @endforeach
